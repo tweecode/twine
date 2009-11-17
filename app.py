92b183d08902e55b45c9477ff21ea976f522b8df
Chris Klimas <klimas@gmail.com>
2009-11-17 00:50:00 +0000

    Version bump to 1.3.5

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ class App (wx.App):
         return scriptPath
     
     NAME = 'Twine'
-    VERSION = '1.3'
+    VERSION = '1.3.5'
     RECENT_FILES = 5
 
 # start things up if we were called directly
