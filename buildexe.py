097d6cd1f8d8168e00561c51701709a84f18723a
stormrose <eturnerx@gmail.com>
2012-04-15 14:39:47 +1200

    Make an .exe and fill a dist/win32 folder - under Python2.7 / Windows
    This only works under Python2.7 for windows. It uses py2exe to build the
    exe and copy it and all the resources Twine needs into the correct
    folder.
    MSVC is a problem and this intial version kludges it's way around.
    
    Change-Id: Ia65d2f24bd13b280d68f166ad7bf1cc2ee6923f4
    Signed-off-by: stormrose <eturnerx@gmail.com>

--- buildexe.py
+++ buildexe.py
@@ -5,34 +5,92 @@
 import sys, os, py2exe
 from distutils.core import setup
 
-manifest = """
+manifest = '''
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1"
-manifestVersion="1.0">
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
 <assemblyIdentity
-    version="0.64.1.0"
+    version="1.3.5.1"
     processorArchitecture="x86"
-    name="Controls"
+    name="%(prog)s"
     type="win32"
 />
-<description>Twine</description>
+<description>%(prog)s Program</description>
 <dependency>
     <dependentAssembly>
         <assemblyIdentity
             type="win32"
-            name="Microsoft.Windows.Common-Controls"
-            version="6.0.0.0"
+            name="Microsoft.VC90.CRT"
+            version="9.0.30729.6161"
             processorArchitecture="X86"
-            publicKeyToken="6595b64144ccf1df"
+            publicKeyToken="1fc8b3b9a1e18e3b"
             language="*"
         />
     </dependentAssembly>
 </dependency>
 </assembly>
-"""
+'''
+
+
+# Force use of py2exe for building Win32.exe
+
+sys.argv.append('py2exe')
+
+# Clear out the dist/win32 directory
+
+for root, dirs, files in os.walk('dist' + os.sep + 'win32', topdown=False):
+    for name in files:
+        os.remove(os.path.join(root, name))
+    for name in dirs:
+        os.rmdir(os.path.join(root, name))
+
 
 # build the exe
 
-setup( windows = [{ 'script':'app.py', \
-                    'icon_resources': [(0x0004, 'icons\\app.ico'), (0x0005, 'icons\\doc.ico')], \
-                    'other_resources': [(24, 1, manifest)] }])
+setup(
+    name = 'Twine',
+    description = 'Twine',
+    version = '1.3.5',
+
+    windows = [{
+        'script': 'app.py',
+        'icon_resources': [(0x0004, 'icons\\app.ico'), (0x0005, 'icons\\doc.ico')],
+        'dest_base': 'twine',
+        'other_resources': [(24, 1, manifest % dict(prog='Twine'))],
+    }],
+
+    data_files = [
+        ('targets' + os.sep + 'jonah',      ['targets' + os.sep + 'jonah'     + os.sep + 'header.html']),
+        ('targets' + os.sep + 'sugarcane',  ['targets' + os.sep + 'sugarcane' + os.sep + 'header.html']),
+        ('targets' + os.sep + 'tw',         ['targets' + os.sep + 'tw'        + os.sep + 'header.html']),
+        ('targets' + os.sep + 'tw2',        ['targets' + os.sep + 'tw2'       + os.sep + 'header.html']),
+        ('icons', [
+            # toolbar icons
+            'icons' + os.sep + 'newpassage.png',
+            'icons' + os.sep + 'zoomin.png',
+            'icons' + os.sep + 'zoomout.png',
+            'icons' + os.sep + 'zoomfit.png',
+            'icons' + os.sep + 'zoom1.png',
+            'icons' + os.sep + 'zoomfit.png',
+            
+            # other icons
+            'icons' + os.sep + 'brokenemblem.png',
+            'icons' + os.sep + 'app.ico',
+        ]),
+                  
+        ('.', ['C:\Windows\winsxs\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.6161_none_50934f2ebcb7eb57\msvcp90.dll']),
+    ],
+
+    options = {
+                  'py2exe': {
+                        'dist_dir': 'dist' + os.sep + 'win32',
+                        'bundle_files': 3,
+                        'optimize': 2,
+                        'ignores': ['_scproxy'],
+                        'dll_excludes': ['w9xpopen.exe'],
+                        'compressed': True,
+                  }
+    },
+    zipfile = 'library.zip',
+)
+
+print 'Check the ./dist/win32 folder'
