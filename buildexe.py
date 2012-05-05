e53c88e119d4d2072b93b7d560211744b6b6a59e
stormrose <eturnerx@gmail.com>
2012-05-05 15:56:14 +1200

    Installer also bundles vcredist_x86.exe - to give proper MSVCR90.dll
    Sorry - only for win32 for now.
    
    Change-Id: I874038eee7f959da4768ff903d49b3c6afb79d2c
    Signed-off-by: stormrose <eturnerx@gmail.com>

--- buildexe.py
+++ buildexe.py
@@ -74,8 +74,6 @@ setup(
             'icons' + os.sep + 'brokenemblem.png',
             'icons' + os.sep + 'app.ico',
         ]),
-                  
-        ('.', ['C:\Windows\winsxs\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.6161_none_50934f2ebcb7eb57\msvcp90.dll']),
     ],
 
     options = {
