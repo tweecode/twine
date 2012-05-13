d4352018055125cd3a72281e5aca23698da230b4
stormrose <eturnerx@gmail.com>
2012-05-13 14:15:44 +1200

    Tooltips now appear on rollover of a widget in story map
    Change-Id: I594c5b2a5fdd846818391af7141ed73a7757101d
    Signed-off-by: stormrose <eturnerx@gmail.com>

--- storypanel.py
+++ storypanel.py
@@ -38,6 +38,7 @@ class StoryPanel (wx.ScrolledWindow):
         self.undoPointer = -1
         self.lastSearchRegexp = None
         self.lastSearchFlags = None
+        self.trackinghover = True;
        
         if (state):
             self.scale = state['scale']
@@ -69,6 +70,9 @@ class StoryPanel (wx.ScrolledWindow):
         self.Bind(wx.EVT_LEFT_DCLICK, self.handleDoubleClick)
         self.Bind(wx.EVT_RIGHT_UP, self.handleRightClick)
         self.Bind(wx.EVT_MIDDLE_UP, self.handleMiddleClick)
+        self.Bind(wx.EVT_ENTER_WINDOW, self.handleHoverStart)
+        self.Bind(wx.EVT_LEAVE_WINDOW, self.handleHoverStop)
+        self.Bind(wx.EVT_MOTION, self.handleHover)
 
     def newWidget (self, title = None, text = '', pos = None, quietly = False):
         """Adds a new widget to the container."""
@@ -776,6 +780,22 @@ class StoryPanel (wx.ScrolledWindow):
                 state['widgets'].append(widget.serialize())
             
         return state
+    
+    def handleHoverStart(self, event):
+        """Turns on hover tracking when mouse enters the frame."""
+        self.trackinghover = True
+
+    def handleHoverStop(self, event):
+        """Turns off hover tracking when mouse leaves the frame."""
+        self.trackinghover = False
+        
+    def handleHover(self, event):
+        if self.trackinghover and not self.draggingWidgets and not self.draggingMarquee:
+            for widget in self.widgets:
+                if widget.getPixelRect().Contains(event.GetPosition()):
+                    self.SetToolTip(wx.ToolTip(widget.passage.title))
+                    return
+            self.SetToolTip(wx.ToolTip(''))
 
     INSET = (10, 10)
     ARROWHEAD_THRESHOLD = 0.5   # won't be drawn below this zoom level
