b5d571a43e9ab00105cf05fcea3b2a8c9bebfd59
greyelf <greyelf@gmail.com>
2014-01-08 16:00:44 +1100

    Added basic Header class support to Twine Changed tiddlywiki to use new class when generating HTML output

--- storyframe.py
+++ storyframe.py
@@ -33,13 +33,13 @@ class StoryFrame (wx.Frame):
         if (state):
             self.buildDestination = state['buildDestination']
             self.saveDestination = state['saveDestination']
-            self.target = state['target']
+            self.setTarget(state['target'].lower())
             self.storyPanel = StoryPanel(self, app, state = state['storyPanel'])
             self.pristine = False
         else:
             self.buildDestination = ''
             self.saveDestination = ''
-            self.target = 'sugarcane'
+            self.setTarget('sugarcane')
             self.storyPanel = StoryPanel(self, app)
         
         # window events
@@ -293,18 +293,14 @@ class StoryFrame (wx.Frame):
         
         storyFormatMenu = wx.Menu()
         storyFormatCounter = StoryFrame.STORY_FORMAT_BASE
-        storyFormatPath = app.targetsPath 
-        	
-        for sfdir in os.listdir(storyFormatPath):
-            try:
-                if os.access(storyFormatPath + sfdir + os.sep + 'header.html', os.R_OK):
-                    storyFormatMenu.Append(storyFormatCounter, sfdir.capitalize(), kind = wx.ITEM_CHECK)
-                    self.Bind(wx.EVT_MENU, lambda e,target=sfdir: self.setTarget(target), id = storyFormatCounter)
-                    self.storyFormats[storyFormatCounter] = sfdir
-                    storyFormatCounter += 1
-            except:
-                pass
-                
+
+        for key in sorted(app.headers.keys()):
+            header = app.headers[key]
+            storyFormatMenu.Append(storyFormatCounter, header.label, kind = wx.ITEM_CHECK)
+            self.Bind(wx.EVT_MENU, lambda e,target=key: self.setTarget(target), id = storyFormatCounter)
+            self.storyFormats[storyFormatCounter] = header
+            storyFormatCounter += 1
+
         if storyFormatCounter:
             storyFormatMenu.AppendSeparator()
        
@@ -1065,6 +1061,7 @@ Modernizr: off
     
     def setTarget (self, target):
         self.target = target
+        self.header = self.app.headers[target]
         
     def updateUI (self, event = None):
         """Adjusts menu items to reflect the current state."""
@@ -1163,9 +1160,8 @@ Modernizr: off
         autoBuildItem.Enable(self.buildDestination != '' and self.storyPanel.findWidget("StoryIncludes") != None)
         
         # Story format submenu
-
         for key in self.storyFormats:
-            self.menus.FindItemById(key).Check(self.target == self.storyFormats[key])
+            self.menus.FindItemById(key).Check(self.target == self.storyFormats[key].id)
         
     def toggleToolbar (self, event = None):
         """Toggles the toolbar onscreen."""
