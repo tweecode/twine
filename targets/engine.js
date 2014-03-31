923e4e3d35a3a3522349e7ea00e96a670ceea306
Leon <L_1_L_0@yahoo.com>
2014-03-31 23:03:36 +1000

    Added class name setting to inline style syntax
    * You can now, in addition to setting styles using the @@ inline style
    syntax, also set a class name for the generated <span>, thus making it a
    shorthand for <span class="..."></span>. Writing @@.name;...@@ will set
    the class of the span to "name". Writing @@.truth.electronic.big;...@@
    will give it the "truth", "electronic" and "big" classes. This can also
    be mixed with regular styles. This was inspired by TiddlyWiki5's
    variation of the TiddlyWiki inline style syntax.
    * Altered History.prototype.saveVariables so that the setter link code
    is simpler. This fixes a bug where variables in setter links were being
    passed by reference rather than value.

--- targets/engine.js
+++ targets/engine.js
@@ -446,15 +446,15 @@ History.prototype.restore = function () {
 };
 
 History.prototype.saveVariables = function(c, el, callback) {
+
+    if (typeof callback == "function") {
+        callback.call(el);
+    }
+
     this.history.unshift({
         passage: c,
         variables: clone(this.history[0].variables)
     });
-    // Setter links
-    if (typeof callback == "function") {
-        callback.call(el);
-        this.history[1] && (this.history[1].linkVars = delta(this.history[1].variables,this.history[0].variables));
-    }
 };
 var restart = History.prototype.restart = function () {
     if (typeof window.history.replaceState == "function") {
@@ -617,7 +617,7 @@ macros["if"] = {
             currentClause = "",
             t = 0,
             nesting = 0;
-		
+        
         for (var i = 0; i < src.length; i++) {
             if (src.substr(i, 9) == "<<endif>>") {
                 nesting--;
@@ -1397,7 +1397,7 @@ Wikifier.formatHelpers = {
     inlineCssHelper: function (w) {
         var s, v, lookaheadMatch, gotMatch,
             styles = [],
-            lookahead = "(?:(" + Wikifier.textPrimitives.anyLetter + "+)\\(([^\\)\\|\\n]+)(?:\\):))|(?:(" + Wikifier.textPrimitives.anyLetter + "+):([^;\\|\\n]+);)",
+            lookahead = Wikifier.styleByCharFormatter.lookahead,
             lookaheadRegExp = new RegExp(lookahead, "mg"),
             hadStyle = false,
             unDash = function (str) {
@@ -1407,13 +1407,16 @@ Wikifier.formatHelpers = {
                 return s.join("");
             };
         
+        styles.className = "";
         do {
             lookaheadRegExp.lastIndex = w.nextMatch;
             lookaheadMatch = lookaheadRegExp.exec(w.source);
             gotMatch = lookaheadMatch && lookaheadMatch.index == w.nextMatch;
             if (gotMatch) {
                 hadStyle = true;
-                if (lookaheadMatch[1]) {
+                if (lookaheadMatch[5]) {
+                    styles.className += lookaheadMatch[5].replace(/\./g," ") + " ";
+                } else if (lookaheadMatch[1]) {
                     s = unDash(lookaheadMatch[1]);
                     v = lookaheadMatch[2];
                 } else {
@@ -1856,21 +1859,28 @@ Wikifier.formatters = [
         }
     }
 },
-{
+(Wikifier.styleByCharFormatter = {
     name: "styleByChar",
     match: "@@",
     terminator: "@@",
-    lookahead: "(?:([^\\(@]+)\\(([^\\)]+)(?:\\):))|(?:([^:@]+):([^;]+);)",
+    lookahead: "(?:([^\\(@]+)\\(([^\\)\\|\\n]+)(?:\\):))|(?:([^\\.:@]+):([^;\\|\\n]+);)|(?:\\.([^;\\|\\n]+);)",
     handler: function (w) {
         var e = insertElement(w.output, "span", null, null, null);
         var styles = Wikifier.formatHelpers.inlineCssHelper(w);
-        if (styles.length == 0)
+        if (styles.length == 0) {
             e.className = "marked";
-        else for (var t = 0; t < styles.length; t++)
-            e.style[styles[t].style] = styles[t].value;
+        }
+        else {
+            for (var t = 0; t < styles.length; t++) {
+                e.style[styles[t].style] = styles[t].value;
+            }
+            if (typeof styles.className == "string") {
+                e.className = styles.className; 
+            }
+        }
         w.subWikify(e, this.terminator);
     }
-},
+}),
 {
     name: "lineBreak",
     match: "\\n",
