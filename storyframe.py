tweecode/twine

commit 65f5a3bd3f08fceff8167484af450bb8c82d7003

Minor StoryPanel and PassageWidget optimisations
* Fixed bug where importing passages with saved position data at a zoom
other than 100% wouldn't work.
* Changed the Redo shortcut on OS X to Cmd-Shift-Z instead of Cmd-Y.
* Caused some things (tiddler links, dragged widgets) to be cached to
slightly improve performance.
* Added Jonah's 'StartPassages' passage name to INFO_PASSAGE.
* Deleted PyRSS2Gen.

storyframe.py
@@ -88,7 +88,8 @@ class StoryFrame (wx.Frame):
         
         fileMenu.AppendSeparator()
         
-        # Import menu
+        # Import submenu
+        
         importMenu = wx.Menu()
 
         importMenu.Append(StoryFrame.FILE_IMPORT_SOURCE, 'Twee Source &Code...')
@@ -98,7 +99,7 @@ class StoryFrame (wx.Frame):
         
         fileMenu.AppendMenu(wx.ID_ANY, '&Import', importMenu)
         
-        # Export menu
+        # Export submenu
         
         exportMenu = wx.Menu()
         
@@ -127,8 +128,13 @@ class StoryFrame (wx.Frame):
         editMenu.Append(wx.ID_UNDO, '&Undo\tCtrl-Z')
         self.Bind(wx.EVT_MENU, lambda e: self.storyPanel.undo(), id = wx.ID_UNDO)
         
-        editMenu.Append(wx.ID_REDO, '&Redo\tCtrl-Y')
-        self.Bind(wx.EVT_MENU, lambda e: self.storyPanel.redo(), id = wx.ID_REDO)
+        if sys.platform == 'darwin':
+            shortcut = 'Ctrl-Shift-Z'
+        else:
+            shortcut = 'Ctrl-Y'
+            
+        editMenu.Append(wx.ID_REDO, '&Redo\t' + shortcut)
+        self.Bind(wx.EVT_MENU, lambda e: self.bodyInput.Redo(), id = wx.ID_REDO)
 
         editMenu.AppendSeparator()
         
@@ -452,12 +458,12 @@ class StoryFrame (wx.Frame):
                 
                 # add passages for each of the tiddlers the TiddlyWiki saw
                 if len(tw.tiddlers):
-                    lastpos = (0, 0)
+                    lastpos = [0, 0]
                     for t in tw.tiddlers:
                         tiddler = tw.tiddlers[t]
                         new = self.storyPanel.newWidget(title = tiddler.title, text = tiddler.text, \
                                                         pos = tiddler.pos if tiddler.pos != None else lastpos, \
-                                                        quietly = True)
+                                                        logicals = True, quietly = True)
                         lastpos = new.pos
                         new.passage.tags = tiddler.tags
                     self.setDirty(True, 'Import')
@@ -482,7 +488,7 @@ class StoryFrame (wx.Frame):
                 
                 # add passages for each of the tiddlers the TiddlyWiki saw
                 if len(tw.tiddlers):
-                    lastpos = (0, 0)
+                    lastpos = [0, 0]
                     for t in tw.tiddlers:
                         tiddler = tw.tiddlers[t]
                         new = self.storyPanel.newWidget(title = tiddler.title, text = tiddler.text, quietly = True, pos = lastpos)

PyRSS2Gen.py
@@ -1,443 +1,0 @@
-"""PyRSS2Gen - A Python library for generating RSS 2.0 feeds."""
-
-__name__ = "PyRSS2Gen"
-__version__ = (1, 0, 0)
-__author__ = "Andrew Dalke <dalke@dalkescientific.com>"
-
-_generator_name = __name__ + "-" + ".".join(map(str, __version__))
-
-import datetime
-
-# Could make this the base class; will need to add 'publish'
-class WriteXmlMixin:
-    def write_xml(self, outfile, encoding = "iso-8859-1"):
-        from xml.sax import saxutils
-        handler = saxutils.XMLGenerator(outfile, encoding)
-        handler.startDocument()
-        self.publish(handler)
-        handler.endDocument()
-
-    def to_xml(self, encoding = "iso-8859-1"):
-        try:
-            import cStringIO as StringIO
-        except ImportError:
-            import StringIO
-        f = StringIO.StringIO()
-        self.write_xml(f, encoding)
-        return f.getvalue()
-
-
-def _element(handler, name, obj, d = {}):
-    if isinstance(obj, basestring) or obj is None:
-        # special-case handling to make the API easier
-        # to use for the common case.
-        handler.startElement(name, d)
-        if obj is not None:
-            handler.characters(obj)
-        handler.endElement(name)
-    else:
-        # It better know how to emit the correct XML.
-        obj.publish(handler)
-
-def _opt_element(handler, name, obj):
-    if obj is None:
-        return
-    _element(handler, name, obj)
-
-
-def _format_date(dt):
-    """convert a datetime into an RFC 822 formatted date
-
-    Input date must be in GMT.
-    """
-    # Looks like:
-    #   Sat, 07 Sep 2002 00:00:01 GMT
-    # Can't use strftime because that's locale dependent
-    #
-    # Isn't there a standard way to do this for Python?  The
-    # rfc822 and email.Utils modules assume a timestamp.  The
-    # following is based on the rfc822 module.
-    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (
-            ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()],
-            dt.day,
-            ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-             "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][dt.month-1],
-            dt.year, dt.hour, dt.minute, dt.second)
-
-        
-##
-# A couple simple wrapper objects for the fields which
-# take a simple value other than a string.
-class IntElement:
-    """implements the 'publish' API for integers
-
-    Takes the tag name and the integer value to publish.
-    
-    (Could be used for anything which uses str() to be published
-    to text for XML.)
-    """
-    element_attrs = {}
-    def __init__(self, name, val):
-        self.name = name
-        self.val = val
-    def publish(self, handler):
-        handler.startElement(self.name, self.element_attrs)
-        handler.characters(str(self.val))
-        handler.endElement(self.name)
-
-class DateElement:
-    """implements the 'publish' API for a datetime.datetime
-
-    Takes the tag name and the datetime to publish.
-
-    Converts the datetime to RFC 2822 timestamp (4-digit year).
-    """
-    def __init__(self, name, dt):
-        self.name = name
-        self.dt = dt
-    def publish(self, handler):
-        _element(handler, self.name, _format_date(self.dt))
-####
-
-class Category:
-    """Publish a category element"""
-    def __init__(self, category, domain = None):
-        self.category = category
-        self.domain = domain
-    def publish(self, handler):
-        d = {}
-        if self.domain is not None:
-            d["domain"] = self.domain
-        _element(handler, "category", self.category, d)
-
-class Cloud:
-    """Publish a cloud"""
-    def __init__(self, domain, port, path,
-                 registerProcedure, protocol):
-        self.domain = domain
-        self.port = port
-        self.path = path
-        self.registerProcedure = registerProcedure
-        self.protocol = protocol
-    def publish(self, handler):
-        _element(handler, "cloud", None, {
-            "domain": self.domain,
-            "port": str(self.port),
-            "path": self.path,
-            "registerProcedure": self.registerProcedure,
-            "protocol": self.protocol})
-
-class Image:
-    """Publish a channel Image"""
-    element_attrs = {}
-    def __init__(self, url, title, link,
-                 width = None, height = None, description = None):
-        self.url = url
-        self.title = title
-        self.link = link
-        self.width = width
-        self.height = height
-        self.description = description
-        
-    def publish(self, handler):
-        handler.startElement("image", self.element_attrs)
-
-        _element(handler, "url", self.url)
-        _element(handler, "title", self.title)
-        _element(handler, "link", self.link)
-
-        width = self.width
-        if isinstance(width, int):
-            width = IntElement("width", width)
-        _opt_element(handler, "width", width)
-        
-        height = self.height
-        if isinstance(height, int):
-            height = IntElement("height", height)
-        _opt_element(handler, "height", height)
-
-        _opt_element(handler, "description", self.description)
-
-        handler.endElement("image")
-
-class Guid:
-    """Publish a guid
-
-    Defaults to being a permalink, which is the assumption if it's
-    omitted.  Hence strings are always permalinks.
-    """
-    def __init__(self, guid, isPermaLink = 1):
-        self.guid = guid
-        self.isPermaLink = isPermaLink
-    def publish(self, handler):
-        d = {}
-        if self.isPermaLink:
-            d["isPermaLink"] = "true"
-        else:
-            d["isPermaLink"] = "false"
-        _element(handler, "guid", self.guid, d)
-
-class TextInput:
-    """Publish a textInput
-
-    Apparently this is rarely used.
-    """
-    element_attrs = {}
-    def __init__(self, title, description, name, link):
-        self.title = title
-        self.description = description
-        self.name = name
-        self.link = link
-
-    def publish(self, handler):
-        handler.startElement("textInput", self.element_attrs)
-        _element(handler, "title", self.title)
-        _element(handler, "description", self.description)
-        _element(handler, "name", self.name)
-        _element(handler, "link", self.link)
-        handler.endElement("textInput")
-        
-
-class Enclosure:
-    """Publish an enclosure"""
-    def __init__(self, url, length, type):
-        self.url = url
-        self.length = length
-        self.type = type
-    def publish(self, handler):
-        _element(handler, "enclosure", None,
-                 {"url": self.url,
-                  "length": str(self.length),
-                  "type": self.type,
-                  })
-
-class Source:
-    """Publish the item's original source, used by aggregators"""
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
-    def publish(self, handler):
-        _element(handler, "source", self.name, {"url": self.url})
-
-class SkipHours:
-    """Publish the skipHours
-
-    This takes a list of hours, as integers.
-    """
-    element_attrs = {}
-    def __init__(self, hours):
-        self.hours = hours
-    def publish(self, handler):
-        if self.hours:
-            handler.startElement("skipHours", self.element_attrs)
-            for hour in self.hours:
-                _element(handler, "hour", str(hour))
-            handler.endElement("skipHours")
-
-class SkipDays:
-    """Publish the skipDays
-
-    This takes a list of days as strings.
-    """
-    element_attrs = {}
-    def __init__(self, days):
-        self.days = days
-    def publish(self, handler):
-        if self.days:
-            handler.startElement("skipDays", self.element_attrs)
-            for day in self.days:
-                _element(handler, "day", day)
-            handler.endElement("skipDays")
-
-class RSS2(WriteXmlMixin):
-    """The main RSS class.
-
-    Stores the channel attributes, with the "category" elements under
-    ".categories" and the RSS items under ".items".
-    """
-    
-    rss_attrs = {"version": "2.0"}
-    element_attrs = {}
-    def __init__(self,
-                 title,
-                 link,
-                 description,
-
-                 language = None,
-                 copyright = None,
-                 managingEditor = None,
-                 webMaster = None,
-                 pubDate = None,  # a datetime, *in* *GMT*
-                 lastBuildDate = None, # a datetime
-                 
-                 categories = None, # list of strings or Category
-                 generator = _generator_name,
-                 docs = "http://blogs.law.harvard.edu/tech/rss",
-                 cloud = None,    # a Cloud
-                 ttl = None,      # integer number of minutes
-
-                 image = None,     # an Image
-                 rating = None,    # a string; I don't know how it's used
-                 textInput = None, # a TextInput
-                 skipHours = None, # a SkipHours with a list of integers
-                 skipDays = None,  # a SkipDays with a list of strings
-
-                 items = None,     # list of RSSItems
-                 ):
-        self.title = title
-        self.link = link
-        self.description = description
-        self.language = language
-        self.copyright = copyright
-        self.managingEditor = managingEditor
-
-        self.webMaster = webMaster
-        self.pubDate = pubDate
-        self.lastBuildDate = lastBuildDate
-        
-        if categories is None:
-            categories = []
-        self.categories = categories
-        self.generator = generator
-        self.docs = docs
-        self.cloud = cloud
-        self.ttl = ttl
-        self.image = image
-        self.rating = rating
-        self.textInput = textInput
-        self.skipHours = skipHours
-        self.skipDays = skipDays
-
-        if items is None:
-            items = []
-        self.items = items
-
-    def publish(self, handler):
-        handler.startElement("rss", self.rss_attrs)
-        handler.startElement("channel", self.element_attrs)
-        _element(handler, "title", self.title)
-        _element(handler, "link", self.link)
-        _element(handler, "description", self.description)
-
-        self.publish_extensions(handler)
-        
-        _opt_element(handler, "language", self.language)
-        _opt_element(handler, "copyright", self.copyright)
-        _opt_element(handler, "managingEditor", self.managingEditor)
-        _opt_element(handler, "webMaster", self.webMaster)
-
-        pubDate = self.pubDate
-        if isinstance(pubDate, datetime.datetime):
-            pubDate = DateElement("pubDate", pubDate)
-        _opt_element(handler, "pubDate", pubDate)
-
-        lastBuildDate = self.lastBuildDate
-        if isinstance(lastBuildDate, datetime.datetime):
-            lastBuildDate = DateElement("lastBuildDate", lastBuildDate)
-        _opt_element(handler, "lastBuildDate", lastBuildDate)
-
-        for category in self.categories:
-            if isinstance(category, basestring):
-                category = Category(category)
-            category.publish(handler)
-
-        _opt_element(handler, "generator", self.generator)
-        _opt_element(handler, "docs", self.docs)
-
-        if self.cloud is not None:
-            self.cloud.publish(handler)
-
-        ttl = self.ttl
-        if isinstance(self.ttl, int):
-            ttl = IntElement("ttl", ttl)
-        _opt_element(handler, "tt", ttl)
-
-        if self.image is not None:
-            self.image.publish(handler)
-
-        _opt_element(handler, "rating", self.rating)
-        if self.textInput is not None:
-            self.textInput.publish(handler)
-        if self.skipHours is not None:
-            self.skipHours.publish(handler)
-        if self.skipDays is not None:
-            self.skipDays.publish(handler)
-
-        for item in self.items:
-            item.publish(handler)
-
-        handler.endElement("channel")
-        handler.endElement("rss")
-
-    def publish_extensions(self, handler):
-        # Derived classes can hook into this to insert
-        # output after the three required fields.
-        pass
-
-    
-    
-class RSSItem(WriteXmlMixin):
-    """Publish an RSS Item"""
-    element_attrs = {}
-    def __init__(self,
-                 title = None,  # string
-                 link = None,   # url as string
-                 description = None, # string
-                 author = None,      # email address as string
-                 categories = None,  # list of string or Category
-                 comments = None,  # url as string
-                 enclosure = None, # an Enclosure
-                 guid = None,    # a unique string
-                 pubDate = None, # a datetime
-                 source = None,  # a Source
-                 ):
-        
-        if title is None and description is None:
-            raise TypeError(
-                "must define at least one of 'title' or 'description'")
-        self.title = title
-        self.link = link
-        self.description = description
-        self.author = author
-        if categories is None:
-            categories = []
-        self.categories = categories
-        self.comments = comments
-        self.enclosure = enclosure
-        self.guid = guid
-        self.pubDate = pubDate
-        self.source = source
-        # It sure does get tedious typing these names three times...
-
-    def publish(self, handler):
-        handler.startElement("item", self.element_attrs)
-        _opt_element(handler, "title", self.title)
-        _opt_element(handler, "link", self.link)
-        self.publish_extensions(handler)
-        _opt_element(handler, "description", self.description)
-        _opt_element(handler, "author", self.author)
-
-        for category in self.categories:
-            if isinstance(category, basestring):
-                category = Category(category)
-            category.publish(handler)
-        
-        _opt_element(handler, "comments", self.comments)
-        if self.enclosure is not None:
-            self.enclosure.publish(handler)
-        _opt_element(handler, "guid", self.guid)
-
-        pubDate = self.pubDate
-        if isinstance(pubDate, datetime.datetime):
-            pubDate = DateElement("pubDate", pubDate)
-        _opt_element(handler, "pubDate", pubDate)
-
-        if self.source is not None:
-            self.source.publish(handler)
-        
-        handler.endElement("item")
-
-    def publish_extensions(self, handler):
-        # Derived classes can hook into this to insert
-        # output after the title and link elements
-        pass

passageframe.py
@@ -16,7 +16,7 @@
 # know what flags to pass to wx.stc.
 #
 
-import sys, os, re, threading, wx, wx.stc
+import sys, os, re, threading, wx, time
 import metrics
 from tweelexer import TweeLexer
 from passagesearchframe import PassageSearchFrame
@@ -72,7 +72,12 @@ class PassageFrame (wx.Frame):
         editMenu.Append(wx.ID_UNDO, '&Undo\tCtrl-Z')
         self.Bind(wx.EVT_MENU, lambda e: self.bodyInput.Undo(), id = wx.ID_UNDO)
 
-        editMenu.Append(wx.ID_REDO, '&Redo\tCtrl-Y')
+        if sys.platform == 'darwin':
+            shortcut = 'Ctrl-Shift-Z'
+        else:
+            shortcut = 'Ctrl-Y'
+            
+        editMenu.Append(wx.ID_REDO, '&Redo\t' + shortcut)
         self.Bind(wx.EVT_MENU, lambda e: self.bodyInput.Redo(), id = wx.ID_REDO)
         
         editMenu.AppendSeparator()
@@ -213,6 +218,7 @@ class PassageFrame (wx.Frame):
         else:
             self.widget.passage.title = 'Untitled Passage'
         self.widget.passage.text = self.bodyInput.GetText()
+		self.widget.passage.modified = time.localtime()
         self.widget.passage.tags = []
         self.widget.clearPaintCache()
         
@@ -236,6 +242,9 @@ class PassageFrame (wx.Frame):
         self.syncTimer = threading.Timer(PassageFrame.PARENT_SYNC_DELAY, reallySync, [self], {})
         self.syncTimer.start()
         
+		# update links/displays lists
+        self.widget.passage.update()
+
         # change our lexer as necessary
         
         self.setLexer()
@@ -262,7 +271,6 @@ class PassageFrame (wx.Frame):
         it is a wx.CommandEvent, and uses the exact text of the menu as the title.
         """
 
-        # this is a bit retarded
         # we seem to be receiving CommandEvents, not MenuEvents,
         # so we can only see menu item IDs
         # unfortunately all our menu items are dynamically generated
@@ -508,7 +516,7 @@ class PassageFrame (wx.Frame):
         incoming = []
         broken = []
         
-        for link in self.widget.passage.links():
+        for link in self.widget.passage.links:
             if len(link) > 0:
                 found = False
                 
@@ -523,7 +531,7 @@ class PassageFrame (wx.Frame):
         # incoming links
 
         for widget in self.widget.parent.widgets:
-            if self.widget.passage.title in widget.passage.links() \
+            if self.widget.passage.title in widget.passage.links \
             and len(widget.passage.title) > 0:
                 incoming.append(widget.passage.title)
                 

passagewidget.py
@@ -33,6 +33,7 @@ class PassageWidget:
         
         if state:
             self.passage = state['passage']
+            self.passage.update()
             self.pos = state['pos']
             self.selected = state['selected']
         else:
@@ -73,7 +74,7 @@ class PassageWidget:
         
         # first, passages we link to
         
-        for link in self.passage.links():
+        for link in self.passage.links:
             widget = self.parent.findWidget(link)
             if widget: dirtyRect = dirtyRect.Union(widget.getPixelRect())
         
@@ -83,7 +84,7 @@ class PassageWidget:
         bridge = [ dirtyRect ]
         
         def addLinkingToRect (widget):
-            if self.passage.title in widget.passage.links():
+            if self.passage.title in widget.passage.links:
                 dirtyRect = bridge[0].Union(widget.getPixelRect())
         
         self.parent.eachWidget(addLinkingToRect)
@@ -149,7 +150,7 @@ class PassageWidget:
     def getBrokenLinks (self):
         """Returns a list of broken links in this widget."""
         brokens = []
-        for link in self.passage.links():
+        for link in self.passage.links:
             if not self.parent.findWidget(link):
                 brokens.append(link)
         return brokens
@@ -206,24 +207,21 @@ class PassageWidget:
         try: self.passageFrame.Destroy()
         except: pass
 
-    def intersectsAny (self):
+    def intersectsAny (self, dragging = False):
         """Returns whether this widget intersects any other in the same StoryPanel."""
-        intersects = False
+        
+        #Enforce positive coordinates
+        if not 'Twine.hide' in self.passage.tags:
+            if ((self.pos[0] < 0) or (self.pos[1] < 0)):
+                return True
         
         # we do this manually so we don't have to go through all of them
         
-        for widget in self.parent.widgets:
+        for widget in (self.parent.notDraggingWidgets if dragging else self.parent.widgets):
             if (widget != self) and (self.intersects(widget)):
-                intersects = True
-                break
+                return True
             
-            #Enforce positive coordinates
-            if not 'Twine.hide' in self.passage.tags:
-                if ((self.pos[0] < 0) or (self.pos[1] < 0)):
-                    intersects = True
-                    break
-
-        return intersects
+        return False
 
     def intersects (self, other):
         """
@@ -304,7 +302,7 @@ class PassageWidget:
         if not self.app.config.ReadBool('fastStoryPanel'):
             gc = wx.GraphicsContext.Create(gc)
         
-        links = self.passage.links()
+        links = self.passage.links
         for link in self.passage.linksAndDisplays():
             if link in dontDraw: continue
                  
@@ -334,6 +332,8 @@ class PassageWidget:
         dc.Blit(rect.x, rect.y, rect.width, rect.height, self.paintBuffer, 0, 0)
     
     def getTitleColorIndex(self):
+        # Find the StartPassages passage
+        
         if any(t.startswith('Twine.') for t in self.passage.tags):
             return 'privateTitleBar'
         elif 'script' in self.passage.tags:

storypanel.py
@@ -32,7 +32,8 @@ class StoryPanel (wx.ScrolledWindow):
         self.snapping = self.app.config.ReadBool('storyPanelSnap')
         self.widgets = []
         self.draggingMarquee = False
-        self.draggingWidgets = False
+        self.draggingWidgets = None
+        self.notDraggingWidgets = None
         self.scrolling = False
         self.undoStack = []
         self.undoPointer = -1
@@ -77,15 +78,15 @@ class StoryPanel (wx.ScrolledWindow):
         self.Bind(wx.EVT_LEAVE_WINDOW, self.handleHoverStop)
         self.Bind(wx.EVT_MOTION, self.handleHover)
         
-    def newWidget (self, title = None, text = '', pos = None, quietly = False):
+    def newWidget (self, title = None, text = '', pos = None, quietly = False, logicals = False):
         """Adds a new widget to the container."""
                 
         # defaults
         
         if not title: title = self.untitledName()
         if not pos: pos = StoryPanel.INSET
-        
-        pos = self.toLogical(pos)
+        if not logicals: qspos = self.toLogical(pos)
+		
         new = PassageWidget(self, self.app, title = title, text = text, pos = pos)
         self.widgets.append(new)
         self.snapWidget(new, quietly)
@@ -461,7 +462,9 @@ class StoryPanel (wx.ScrolledWindow):
         it passes control to us so that we can move all selected widgets at once.
         """
         if not self.draggingWidgets:
-            self.draggingWidgets = True
+            # cache the sets of dragged vs not-dragged widgets
+            self.draggingWidgets = []
+            self.notDraggingWidgets = []
             self.clickedWidget = clickedWidget
             self.actuallyDragged = False
             self.dragCurrent = event.GetPosition()
@@ -472,7 +475,10 @@ class StoryPanel (wx.ScrolledWindow):
             
             for widget in self.widgets:
                 if widget.selected:
+                    self.draggingWidgets.append(widget)
                     widget.predragPos = widget.pos
+                else:
+                    self.notDraggingWidgets.append(widget)
             
             # grab mouse focus
             
@@ -501,10 +507,8 @@ class StoryPanel (wx.ScrolledWindow):
             
             goodDrag = True
             
-            # TODO: optimize this loop
-            
-            for widget in self.widgets:
-                if widget.selected and widget.intersectsAny():
+            for widget in self.draggingWidgets:
+                if widget.intersectsAny(dragging = True):
                     goodDrag = False
                     break
 
@@ -540,15 +544,14 @@ class StoryPanel (wx.ScrolledWindow):
             self.oldDirtyRect = dirtyRect
             self.Refresh(True, dirtyRect)
         else:
-            self.draggingWidgets = False
 
             if self.actuallyDragged:
                 # is this a bad drag?
     
                 goodDrag = True
                 
-                for widget in self.widgets:
-                    if widget.selected and widget.intersectsAny():
+                for widget in self.draggingWidgets:
+                    if widget.intersectsAny(dragging = True):
                         goodDrag = False
                         break
                     
@@ -558,11 +561,14 @@ class StoryPanel (wx.ScrolledWindow):
                     self.parent.setDirty(True, action = 'Move')
                     self.resize()
                 else:
-                    for widget in self.widgets:
-                        if widget.selected:
-                            widget.pos = widget.predragPos
-                            widget.setDimmed(False)
+                    for widget in self.draggingWidgets:
+                        widget.pos = widget.predragPos
+                        widget.setDimmed(False)
                     self.Refresh()
+					
+				self.draggingWidgets = None
+                self.notDraggingWidgets = None
+				
             else:
                 # change the selection
                 self.clickedWidget.setSelected(True, not event.ShiftDown())

commit 8fc835dfb344d384da4801b22471dbd6397d375f

Fix redo support in StoryPanel
Found because it tripped PyLint's 'no-member' rule.
This got broken in commit 65f5a3bd.

storyframe.py
@@ -138,7 +138,7 @@ class StoryFrame(wx.Frame):
             shortcut = 'Ctrl-Y'
 
         editMenu.Append(wx.ID_REDO, '&Redo\t' + shortcut)
-        self.Bind(wx.EVT_MENU, lambda e: self.bodyInput.Redo(), id=wx.ID_REDO)
+        self.Bind(wx.EVT_MENU, lambda e: self.storyPanel.redo(), id=wx.ID_REDO)
 
         editMenu.AppendSeparator()
 